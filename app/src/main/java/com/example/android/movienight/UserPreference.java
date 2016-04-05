e70d8086919deb3a11ecc30034eed5e0e1cdc721
Sam <saumya.bhatnagar.sb@gmail.com>
2016-04-05 12:24:02 +0530

    Restructered the project

--- app/src/main/java/com/example/android/movienight/UserPreference.java
+++ app/src/main/java/com/example/android/movienight/UserPreference.java
@@ -3,6 +3,8 @@ package com.example.android.movienight;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -28,8 +30,35 @@ public class UserPreference extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.actress_Spinner);
         adapter = ArrayAdapter.createFromResource(this,
                 R.array.actresses_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void checkboxes(View view){
+        switch(view.getId()){
+            case R.id.action:
+                break;
+            case R.id.adventure:
+                break;
+            case R.id.thriller:
+                break;
+            case R.id.comedy:
+                break;
+            case R.id.romance:
+                break;
+            case R.id.animation:
+                break;
+            case R.id.family:
+                break;
+            case R.id.sci_fi:
+                break;
+            case R.id.mystery:
+                break;
+            default:finish();
+                Log.e("in User preference", "Incorrect checkbox chosen");
+                break;
+        }
     }
 }
